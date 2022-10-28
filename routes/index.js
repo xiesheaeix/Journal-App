37497f3c8e7a1f25b5d33b78a718d9a49c22c13b
xiesheaeix <sheae123@gmail.com>
2022-10-28 12:17:53 -0700

    oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    prompt: 'select_account'
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    // Change to what's best for YOUR app
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+    // Change path for your "landing" page
+    res.redirect('/');
+  });
 });
 
 module.exports = router;

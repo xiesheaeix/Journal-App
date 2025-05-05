1e132cacd06316ca7ea777a06735d641b4c9cae6
Shea Alvarado <sheae123@gmail.com>
2025-05-05 01:20:56 -0700

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -14,13 +14,28 @@ function index(req, res) {
 }
 
 function show(req, res) {
-  let user = req.user;
-    User.findById(req.params.id, function(err, displayUser) { 
-      Post.find({user: req.params.id, private: false}, function(err, posts) { 
-        res.render('users/show',{ displayUser, posts, user });
+  // We fetch the user profile based on the ID parameter in the route.
+  User.findById(req.params.id, function(err, displayUser) {
+    if (err || !displayUser) {
+      return res.status(404).send('User not found'); // Handle case where user is not found
+    }
+
+    // Fetch the posts belonging to this user
+    Post.find({ user: req.params.id, private: false }, function(err, posts) {
+      if (err) {
+        return res.status(500).send('Error fetching posts');
+      }
+
+      // Pass the displayUser, posts, and current user to the view
+      res.render('users/show', {
+        displayUser, // User whose profile is being viewed
+        posts,       // Public posts from the user
+        user: req.user // The logged-in user
+      });
     });
   });
 }
+
 
 function private(req, res) {
   Post.find({user: req.user._id, private: true}, function(err, posts) { 
